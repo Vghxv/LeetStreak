9a417e93e408771f7b7948b5c325083313a83dea
Vincent <hankggg2003xd@gmail.com>
2024-09-04 15:14:24 +0800

    Implement simplifyPath2 function in stack/71.cpp

--- stack/71.cpp
+++ stack/71.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <sstream>
 #include <stack>
 #include <string>
 using namespace std;
@@ -36,24 +37,57 @@ public:
         }
         return result.empty() ? "/" : result;
     }
+    string simplifyPath2(string path)
+    {
+        stack<string> st;
+        string tmp;
+        stringstream ss(path);
+        while (getline(ss, tmp, '/')) {
+            if (tmp == "..") {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            }
+            else if (!tmp.empty() && tmp != ".") {
+                st.push(tmp);
+            }
+        }
+        string result;
+        while (!st.empty()) {
+            result = "/" + st.top() + result;
+            st.pop();
+        }
+        return result.empty() ? "/" : result;
+    }
 };
 
 class Testing : public testing::Test {
+protected:
+    int testnum = 1;
+
 public:
     Solution s;
+    string (Solution::*func[2])(string) = {&Solution::simplifyPath,
+                                           &Solution::simplifyPath2};
 };
 
-TEST_F(Testing, t1) { EXPECT_EQ(s.simplifyPath("/home/"), "/home"); }
+TEST_F(Testing, t1) { EXPECT_EQ((s.*func[testnum])("/home/"), "/home"); }
 
-TEST_F(Testing, t2) { EXPECT_EQ(s.simplifyPath("/../"), "/"); }
+TEST_F(Testing, t2) { EXPECT_EQ((s.*func[testnum])("/../"), "/"); }
 
-TEST_F(Testing, t3) { EXPECT_EQ(s.simplifyPath("/home//foo/"), "/home/foo"); }
+TEST_F(Testing, t3)
+{
+    EXPECT_EQ((s.*func[testnum])("/home//foo/"), "/home/foo");
+}
 
-TEST_F(Testing, t4) { EXPECT_EQ(s.simplifyPath("/a/./b/../../c/"), "/c"); }
+TEST_F(Testing, t4) { EXPECT_EQ((s.*func[testnum])("/a/./b/../../c/"), "/c"); }
 
-TEST_F(Testing, t5) { EXPECT_EQ(s.simplifyPath("/a/../../b/../c//.//"), "/c"); }
+TEST_F(Testing, t5)
+{
+    EXPECT_EQ((s.*func[testnum])("/a/../../b/../c//.//"), "/c");
+}
 
 TEST_F(Testing, t6)
 {
-    EXPECT_EQ(s.simplifyPath("/a//b////c/d//././/.."), "/a/b/c");
+    EXPECT_EQ((s.*func[testnum])("/a//b////c/d//././/.."), "/a/b/c");
 }
