09dd7e050c1f8cdde499ca4ac8cff2212d2e97f0
Vincent <hankggg2003xd@gmail.com>
2024-09-15 13:54:37 +0800

    Refactor buildTree function to handle empty preorder vector

--- binary_tree_general/105.cpp
+++ binary_tree_general/105.cpp
@@ -29,24 +29,21 @@ public:
     inline TreeNode *build(vector<int> &preorder, vector<int> &inorder,
                            int rootIndex, int left, int right)
     {
+        if (left > right) {
+            return nullptr;
+        }
         int index = left;
         while (inorder[index] != preorder[rootIndex]) {
             index++;
         }
-        return new TreeNode(preorder[rootIndex],
-                            index > left ? build(preorder, inorder,
-                                                 rootIndex + 1, left, index - 1)
-                                         : nullptr,
-                            index < right ? build(preorder, inorder,
-                                                  rootIndex + index - left + 1,
-                                                  index + 1, right)
-                                          : nullptr);
+        return new TreeNode(
+            preorder[rootIndex],
+            build(preorder, inorder, rootIndex + 1, left, index - 1),
+            build(preorder, inorder, rootIndex + index - left + 1, index + 1,
+                  right));
     }
     TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
     {
-        if (preorder.size() == 0) {
-            return nullptr;
-        }
         return build(preorder, inorder, 0, 0, inorder.size() - 1);
     }
 };
