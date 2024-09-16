14cf2c7eb7b0661b13252cbd98e61a2b7825387d
Vincent <hankggg2003xd@gmail.com>
2024-09-16 16:04:15 +0800

    Refactor lowestCommonAncestor function for improved readability and performance

--- binary_tree_general/236.cpp
+++ binary_tree_general/236.cpp
@@ -7,9 +7,19 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
         if (root == nullptr) {
             return nullptr;
         }
@@ -18,7 +28,7 @@ public:
         }
         TreeNode *left = lowestCommonAncestor(root->left, p, q);
         TreeNode *right = lowestCommonAncestor(root->right, p, q);
-        
+
         if (left != nullptr && right != nullptr) {
             return root;
         }
