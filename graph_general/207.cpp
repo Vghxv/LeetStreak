dd8ce1eac9c399ed10db0a952af7d39243216bdb
Vincent <hankggg2003xd@gmail.com>
2024-09-23 20:55:12 +0800

    Refactor graph traversal algorithm and add unit tests

--- graph_general/207.cpp
+++ graph_general/207.cpp
@@ -3,49 +3,36 @@
 using namespace std;
 class Solution {
 public:
-    inline void fill_graph(vector<vector<int>> &prerequisites,
-                           vector<vector<int>> &graph)
-    {
-        for (auto &p : prerequisites) {
-            graph[p[1]].push_back(p[0]);
-        }
-    }
-    inline bool dfs(int node, vector<vector<int>> &graph, vector<bool> &visited,
-                    vector<bool> &stack)
-    {
-        if (!visited[node]) {
-            visited[node] = true;
-            stack[node] = true;
-            for (int neighbor : graph[node]) {
-                if (!visited[neighbor] &&
-                    dfs(neighbor, graph, visited, stack)) {
-                    return true;
-                }
-                else if (stack[neighbor]) {
-                    return true;
-                }
-            }
-        }
-        stack[node] = false;
-        return false;
-    }
-    inline bool solve(int numCourses, vector<vector<int>> &graph)
-    {
-        vector<bool> visited(numCourses, false);
-        vector<bool> stack(numCourses, false);
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i] && dfs(i, graph, visited, stack)) {
-                return false;
-            }
-        }
-        return true;
-    }
     bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
     {
-        vector<vector<int>> graph(numCourses);
-        fill_graph(prerequisites, graph);
-        return solve(numCourses, graph);
+        vector<vector<int>> neighbors(numCourses);
+        vector<int> inDegree(numCourses, 0);
+
+        for (auto &pre : prerequisites) {
+            neighbors[pre[1]].push_back(pre[0]);
+            inDegree[pre[0]]++;
+        }
+
+        queue<int> zeroDegree;
+        for (int i = 0; i < numCourses; ++i) {
+            if (inDegree[i] == 0) {
+                zeroDegree.push(i);
+            }
+        }
+
+        int visitedNodes = 0;
+        while (!zeroDegree.empty()) {
+            int node = zeroDegree.front();
+            zeroDegree.pop();
+            visitedNodes++;
+            for (int neighbor : neighbors[node]) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    zeroDegree.push(neighbor);
+                }
+            }
+        }
+        return visitedNodes == numCourses;
     }
 };
 
